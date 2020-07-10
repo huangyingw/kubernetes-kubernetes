38c0094d625ce29f90901cf0c47e1bc1d3cc90c2
Ying Huang <huangyingw@gmail.com>
2020-07-10 12:27:03 -0400

    n

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ cd "$SCRIPTPATH"
 
 make \
     GOGCFLAGS="-e" \
-    GOGCFLAGS="-N -l"
-
-cp -fv ./_output/local/bin/linux/amd64/kubectl /usr/bin/kubectl
-cp -fv ./_output/local/bin/linux/amd64/kubeadm /usr/bin/kubeadm
+    GOGCFLAGS="-N -l" && \
+    cp -fv ./_output/local/bin/linux/amd64/kubectl /usr/bin/kubectl && \
+    cp -fv ./_output/local/bin/linux/amd64/kubeadm /usr/bin/kubeadm
